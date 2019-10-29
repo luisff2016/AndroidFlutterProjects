91cb2d27e703a8abf52a46270331a105b1b990e6
luisff2016 <luisff@dcomp.ufs.br>
2019-10-29 00:24:39 -0300

    ajustar subtitulo

--- lib/utils/star_wars_styles.dart
+++ lib/utils/star_wars_styles.dart
@@ -4,5 +4,5 @@ class StarWarsStyles {
   static final double titleFontSize = 16.0;
   static final double subTitleFontSize = 14.0;
   static final Color titleColor = Colors.black.withAlpha(220);
-  static final Color subTitleColor = Colors.black87;
+  static final Color subTitleColor = Colors.black54;
 }
